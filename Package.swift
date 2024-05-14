c4b4b7e68022f6693b4d85e5d9a5042bc7014d01
Ryan McKinney <rydamckinney@gmail.com>
2024-05-14 12:55:19 -0400

    Sym Deps

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/nathantannar4/Engine", from: "1.5.1"),
+        .package(url: "https://github.com/symbiose-technologies/Engine", from: "1.5.1"),
     ],
     targets: [
         .target(
